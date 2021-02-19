fbfae81f54df37fd4534d07d3c329a9c2cfbfa28
Bojan <bojan.bosnic97@gmail.com>
2021-02-19 14:12:15 +0100

    first commit

---
+++ src/js/no-js-files.js
@@ -0,0 +1,10 @@
+// importing all files what is not javascript
+
+// import '../img/webpack4.jpg';
+// import '../img/bg.png';
+import '../scss/index.scss';
+import '../img/icons/favicon.ico';
+import '../img/icons/favicon.png';
+import '../../.gitkeep';
+import '../.htaccess';
+
